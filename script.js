30cd6bd1aa060dc4eda6fa33b4b5407d3ad4d49c
MariamAhmadElharty <8mariamahmad8@gmail.com>
2024-12-13 05:45:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded" , function(){
+    let button_click=document.getElementById("aboutme");
+    button_click.addEventListener("click" , click);
+    function click(){
+        alert("My name is Mariam Ahmad and these are my handmade crafts!");
+    }
+ 
+});
